7ce54d29d01a82556ef184d87315b4f6123154df
Anmol <anmolbisht432@gmail.com>
2025-11-12 22:32:56 +0530

    anmol

--- SmartShop_Gen_2/CSV_to_SQlite_Converter.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import pandas as pd
-
-path = r"C:\Users\AKSHAT\Desktop\DBMS_PBL\SmartShop\SmartShop_Gen_2\dataset2.csv"
-df = pd.read_csv(path)
-
-conn = sqlite3.connect("my_database.db")
-df.to_sql("my_table", conn, if_exists="append", index=False)
-conn.close()
